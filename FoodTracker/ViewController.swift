b212c4b7267dca0b3866f7b15ee95a0aa220421e
김종권 <jongkwon.kim@code42.ai>
2020-06-18 20:36:42 +0900

    view controller

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -15,6 +15,13 @@ class ViewController: UIViewController {
     @IBAction func setText(_ sender: Any) {
         nameLbl.text = "default text"
     }
+    @IBOutlet weak var photo: UIImageView!
+    @IBAction func tapImage(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        present(picker, animated: true)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +30,18 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let img = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {return}
+        photo.image = img
+        dismiss(animated: true, completion: nil)
+    }
 }
 
 extension ViewController: UITextFieldDelegate {
