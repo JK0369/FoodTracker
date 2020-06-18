2bc4bb8c7bf02b48bd3710e6e0f166204532b6ed
김종권 <jongkwon.kim@code42.ai>
2020-06-18 20:19:37 +0900

    start

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by A10152 on 2020/06/18.
+//  Copyright © 2020 A10152. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
